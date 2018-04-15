ee7ef199cef6eba383e771c9ba3ec00f27508982
dahby <dstoll243@gmail.com>
2018-04-15 16:06:39 -0700

    finished book and examples

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,21 +1,62 @@
-const exec = require('child_process').exec;
+const querystring = require('querystring');
+const fs = require('fs');
 
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec('ls -lah', function (error, stdout, stderr) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write(stdout);
+  const body = '<html>' +
+    '<head>' +
+    '<meta http-equiv="Content-Type" content="text/html; ' + 'charset=UTF-8" />' +
+    '</head>' +
+    '<body>' +
+    '<form action="/upload" method="post">' +
+    '<textarea name="text" rows="20" cols="60"></textarea>' +
+    '<input type="submit" value="Upload file" />' +
+    '</form>' +
+    '</body>' +
+    '</html>';
+
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(body);
+  response.end();
+}
+
+function upload(response, request) {
+  console.log("Request handler 'upload' was called.");
+
+  const form = new formidable.IncomingForm();
+  console.log('about to parse');
+  form.parse(request, (error, fields, files) => {
+    console.log('parsing done');
+
+    fs.rename(files.upload.path, '/tmp/test.png', (err) => {
+      if (err) {
+        fs.unlink('/tmp/test.png');
+        fs.rename(files.upload.path, '/tmp/test.png');
+      }
+    });
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('received image:<br/>');
+    response.write("<img src='/show' />");
     response.end();
   });
 }
 
-function upload(response) {
-  console.log("Request handler 'upload' was called.");
-  response.writeHead(200, { 'Content-Type': 'text/plain' });
-  response.write('Hello Upload');
-  response.end();
+function show(response, postData) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile('/tmp/test.png', 'binary', (error, file) => {
+    if (error) {
+      response.writeHead(500, { 'Content-Type': 'text/plain' });
+      response.write(`${error}\n`);
+      response.end();
+    } else {
+      response.writeHead(200, { 'Content-Type': 'image/png' });
+      response.write(file, 'binary');
+      response.end();
+    }
+  });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
