3694b696052124cf0c1d34267ac47d4d025f0c20
dahby <dstoll243@gmail.com>
2018-04-15 14:35:37 -0700

    handlers with non-blocking operations

--- server.js
+++ server.js
@@ -1,16 +1,12 @@
 const http = require('http');
 const url = require('url');
 
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     const pathname = url.parse(request.url).pathname;
-    console.log('Request for ' + pathname + ' received.');
+    console.log(`Request for ${pathname} received.`);
 
-    route(pathname);
-
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World');
-    response.end();
+    route(handle, pathname, response);
   }
 
   http.createServer(onRequest).listen(8888);
